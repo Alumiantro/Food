0cee069e5aaee4515ddb9889df8135e0290f62ac
Adelya <zaikamarta@mail.ru>
2022-07-02 21:57:11 +0300

    implemented timer using js

--- js/script.js
+++ js/script.js
@@ -30,4 +30,50 @@ window.addEventListener('DOMContentLoaded', () => {
     });
     hideContent();
     showContent();
+
+    //Timer
+    const deadline = '2022-09-28'; //конечная цель
+    function getTimeRemaining(endTime) {
+        const t = Date.parse(endTime) - Date.parse(new Date());
+        const day = Math.floor(t / (1000 * 60 * 60 * 24));
+        const minutes = Math.floor(t / (1000 * 60 * 60) % 60);
+        const hours = Math.floor(t / (1000 * 60 * 60) % 24);
+        const seconds = Math.floor((t / 1000) % 60);
+        return {
+            "total": t,
+            "day": day,
+            "minutes": minutes,
+            "hours": hours,
+            "seconds": seconds
+        };
+    }
+
+    function addZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(endTime) {
+        const days = document.querySelector('#days');
+        const hours = document.querySelector('#hours');
+        const minutes = document.querySelector('#minutes');
+        const seconds = document.querySelector('#seconds');
+        const timeInterval = setInterval(updateClock, 1000);
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endTime);
+            minutes.innerHTML = addZero(t.minutes);
+            days.innerHTML = addZero(t.day);
+            hours.innerHTML = addZero(t.hours);
+            seconds.innerHTML = addZero(t.seconds);
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    setClock(deadline);
 });
